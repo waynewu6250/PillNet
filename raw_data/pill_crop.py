18c0d801a41b1e3e4d7dc7cc864707111b7d35fe
waynewu6250 <waynewu6250@gmail.com>
2019-06-05 15:16:15 +0800

    Add

---
+++ raw_data/pill_crop.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import cv2
+from PIL import Image
+import os
+
+data = pd.read_csv("../data/pill_labels.csv")
+
+for i in range(len(data)):
+    
+    filename = data.loc[i]["filename"]
+    xmin = data.loc[i]["xmin"]
+    ymin = data.loc[i]["ymin"]
+    xmax = data.loc[i]["xmax"]
+    ymax = data.loc[i]["ymax"]
+    counter = data.loc[i]["data_num"]
+    
+    path = os.path.join("./tests",filename)
+    if os.path.exists(path):
+        im = Image.open(path)
+        region = im.crop((xmin, ymin, xmax, ymax))
+        region.save("./images_crop/"+filename.split('.')[0]+"-{}.jpg".format(counter))
